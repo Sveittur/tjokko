bb01372007157a1db4bdc05c01662cb553e48575
Sveittur <doktordanni@gmail.com>
2023-07-27 19:38:17 +0000

    popup

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const readMoreBtn = document.querySelector(".readMoreBtn");
 const readMoreTxt = document.querySelector(".moreText");
 const readLessBtn = document.querySelector(".readLessBtn");
+const closeButton = document.querySelector(".closeButton");
+const registerButton = document.querySelector(".registerButton");
+const popup = document.querySelector(".popup");
 console.log(readMoreBtn);
 console.log(readMoreTxt);
 
@@ -14,4 +17,12 @@ readLessBtn.addEventListener('click', (e)=>{
     readMoreTxt.classList.toggle('showMoreText');
     readMoreBtn.style.display = "inline";
     readLessBtn.style.display = "none";
+})
+
+registerButton.addEventListener('click', (e)=>{
+    popup.style.display = "flex";
+})
+
+closeButton.addEventListener('click', (e)=>{
+    popup.style.display = "none";
 })
